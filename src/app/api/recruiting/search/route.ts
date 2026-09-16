dd30e3dc8290a9f3e2021a170bf3625798eaa88f
sameer0075 <sameerbutt151@gmail.com>
2026-09-16 23:38:44 +0500

    admin superadmin removed

--- src/app/api/recruiting/search/route.ts
+++ src/app/api/recruiting/search/route.ts
@@ -18,6 +18,7 @@ export async function GET(req: Request) {
     profileVisibility: true,
     user: {
       status: "ACTIVE",
+      role: { notIn: ["ADMIN", "SUPER_ADMIN"] },
       NOT: {
         AND: [
           {
